e5d1a906b3d3c294873fa13652701337f03b7c79
chengkehan <ckhxlt@163.com>
2017-04-09 18:26:02 +0800

    dev progress commit

---
+++ Assets/GPUSkinning/Scripts/GPUSkinningPlayerMono.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPUSkinningPlayerMono : MonoBehaviour
+{
+    public GPUSkinningAnimation anim = null;
+
+    public Mesh mesh = null;
+
+    public Material mtrl = null;
+
+    private GPUSkinningPlayer player = null;
+    public GPUSkinningPlayer Player
+    {
+        get
+        {
+            return player;
+        }
+    }
+
+    private void Start()
+    {
+        player = new GPUSkinningPlayer(gameObject, anim, mesh, new Material(mtrl));
+
+        if (anim != null && anim.clips != null && anim.clips.Length > 0)
+        {
+            player.Play(anim.clips[0].name);
+        }
+    }
+
+    private void Update()
+    {
+        player.Update(Time.deltaTime);
+    }
+}
