a5790fb1069c86ad184877be2588d3b4a0ea8d69
jimCheng <chengkehan@gmail.com>
2017-05-12 16:27:11 +0800

    dev progress commit

---
+++ Assets/GPUSkinning/Scripts/GPUSkinningCycleList.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPUSkinningCycleList<T>
+{
+    private GPUSkinningBetterList<T> list = null;
+
+    private int pointer = 0;
+
+    public GPUSkinningCycleList(int bufferIncrement)
+    {
+        list = new GPUSkinningBetterList<T>(bufferIncrement);
+    }
+
+    public void Set(T[] data)
+    {
+        list.Clear();
+        list.AddRange(data);
+        pointer = 0;
+    }
+
+    public void Next()
+    {
+        ++pointer;
+        if(pointer >= list.size)
+        {
+            pointer = 0;
+        }
+    }
+
+    public T Peek()
+    {
+        if(pointer >= list.size)
+        {
+            return default(T);
+        }
+        return list[pointer];
+    }
+}
